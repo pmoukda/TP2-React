9171e0265181306b729eb9aabf2601bfd38b5bd5
pmoukda <moukda_p@hotmail.com>
2025-10-03 11:08:29 -0400

    Ajouter du CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div>
+        <div className="text-center p-3 bg-black text-white">
             <p>React &copy; SunVacay.2025 -Tous droits réservés.</p>
         </div>
     )
