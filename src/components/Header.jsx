f056db3e91dfb6abe8fe77760e2cc9ec8c5b9704
pmoukda <moukda_p@hotmail.com>
2025-10-05 00:57:45 -0400

    Changer contenu de Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,14 @@
 import { Link } from 'react-router-dom'
-// import { useLocation } from 'react-router-dom';
-const Header = ({logo = "SunVacay"}) => {
-       return (
-        <header className="">
-            <nav className='container mx-auto flex justify-between items-center px-4'>
-                <Link to="/">{logo}</Link>
-                <ul>
-                    <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/apropos">À Propos</Link></li>
-                    <li><Link to="/forfaits">Les Forfaits</Link></li>
-                </ul>
-            </nav>
-        </header>
+
+const Header = ({$title="Partez à l'aventure avec SunVacay votre agence de voyage par excellence!"}) => {
+    return (
+        <div className='relative'>
+            <img className="w-full h-125 object-cover object-center mb-4" src="/img/ile-grecque.jpg" alt="Image" />
+            <div className="flex flex-col justify-center items-center absolute inset-0 bg-black/40">
+                <h1 className='text-4xl font-bold text-white'>{$title}</h1>
+                <Link className='btn-yellow' to="/forfaits">Découvrir nos forfaits</Link>
+            </div>
+        </div>
     );
-}
+ }
 export default Header;
