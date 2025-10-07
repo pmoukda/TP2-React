df7b5e39bf94a4c2d05448a986a13a3494f3e31c
pmoukda <moukda_p@hotmail.com>
2025-10-06 22:59:55 -0400

    Corriger un bogue

--- src/components/LesForfaits.jsx
+++ src/components/LesForfaits.jsx
@@ -1,12 +1,12 @@
 // import { Link } from 'react-router-dom';
 import Forfait from './Forfait';
 
-
-const LesForfaits = ({forfaits, ondelete, onEdit}) => {
+// Identifier chaque élément dans la liste par son id
+const LesForfaits = ({forfaits, onDelete, onEdit }) => {
     return (
-       <div className="max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-4 bg-blue-100">
+       <div className="max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6 bg-blue-100">
             {forfaits.map((forfait)=>(
-                <Forfait key={forfait.id} forfait={forfait} ondelete={ondelete} onEdit={onEdit}/>
+                <Forfait key={forfait.id} forfait={forfait} onDelete={onDelete} onEdit={onEdit}/>
             ))}
         </div>
     )
